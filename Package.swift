6afde6121716f9af5051b877e0b5309e4e939981
Suyeol Jeon <devxoul@gmail.com>
2019-03-29 16:39:24 +0900

    Use Swift 5 in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
   name: "Stubber",
+  platforms: [
+    .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+  ],
   products: [
     .library(name: "Stubber", targets: ["Stubber"]),
   ],
   targets: [
     .target(name: "Stubber"),
     .testTarget(name: "StubberTests", dependencies: ["Stubber"]),
-  ]
+  ],
+  swiftLanguageVersions: [.v5]
 )
